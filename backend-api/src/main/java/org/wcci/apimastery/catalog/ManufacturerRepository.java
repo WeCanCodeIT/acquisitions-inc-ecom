fc7c2a2a80d1308df5d671035425984812f1b57b
Columbus Instructor <instructor@wecancodeit.org>
2020-03-13 11:26:50 -0400

    Buiding a controller in ONE COMMIT
    -Bad
    - Bad
    - Bad

--- backend-api/src/main/java/org/wcci/apimastery/catalog/ManufacturerRepository.java
+++ backend-api/src/main/java/org/wcci/apimastery/catalog/ManufacturerRepository.java
@@ -3,4 +3,5 @@ package org.wcci.apimastery.catalog;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ManufacturerRepository extends CrudRepository<Manufacturer, Long> {
+
 }
